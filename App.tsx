db3088f2aef13e282722ce96f2a875388c4f6e47
pasan <pasanwijekoon673@gmail.com>
2023-01-31 06:59:21 +0000

    first commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button,SafeAreaView } from "react-native";
+
+function App(){
+
+  const b = (
+    
+    <SafeAreaView>
+    <Button title="hh"/>
+
+    </SafeAreaView>
+  );
+ return b;
+}
+export default App;
